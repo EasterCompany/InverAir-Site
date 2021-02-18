2e27f3469810375b5e8c18f8c6fadf5c4c57fa64
oceaster <oceaster@gmail.com>
2021-02-18 00:26:29 +0000

    added service workers

--- core/views.py
+++ core/views.py
@@ -15,3 +15,11 @@ def manifestJSON(req, *args, **kwargs):
 
 def asset_manifest(req, *args, **kwargs):
     return render(req, 'asset-manifest.json', content_type='application/json')
+
+
+def service_worker(req, *args, **kwargs):
+    return render(req, 'service-worker.js', content_type="application/x-javascript")
+
+
+def service_worker_map(req, *args, **kwargs):
+    return render(req, 'service-worker.js.map', content_type="application/x-javascript")
--- web/urls.py
+++ web/urls.py
@@ -9,8 +9,13 @@ urlpatterns = [
 
     # Core App
     path('', index),
+    path('index.html', index),
     path('robots.txt', robots),
     path('manifest.json', manifestJSON),
     path('asset-manifest.json', asset_manifest),
+
+    # Service Worker
+    path('service-worker.js', service_worker),
+    path('service-worker.js.map', service_worker_map),
 
 ]
